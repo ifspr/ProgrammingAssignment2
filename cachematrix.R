088001eed05e541671a56d1b075dc722b359f1b0
ifspr <stephanie_rusli@hotmail.com>
2015-07-25 19:08:45 +0200

    Completed cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix takes a (square, invertible) matrix as an input
+##   and creates a list containing functions to:
+##   1. set the value of the matrix
+##   2. get the value of the matrix
+##   3. set the inverse of the matrix
+##   4. get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve returns the inverse of a given matrix. 
+## It first checks if the matrix inverse has been computed. 
+## If so, it retrieves the inverse from the cache. 
+## Otherwise, it computes the inverse of the matrix and 
+##   stores that in the cache via the setinv function. 
+## The matrix is assumed to be square and invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data,...)
+        x$setinv(inv)
+        inv
 }
